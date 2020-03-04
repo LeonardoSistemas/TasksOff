e16e00212da68cbb8e6ac24bffbf2fbca1d88dc8
LeonardoSistemas <43937635+LeonardoSistemas@users.noreply.github.com>
2020-03-04 18:09:14 -0300

    Atualização das funções
    Criar funções para atualizar os pendentes e visualização dos pendentes

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,76 @@
 import React, { Component } from "react";
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform
+} from "react-native";
 import TodayImage from "../assets/imgs/today.jpg";
 import moment from "moment";
 import "moment/locale/pt-br";
 import commonStyles from "../commonStyles";
 import Task from "../components/Tasks";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default class TaskList extends Component {
+  state = {
+    showDoneTasks: true,
+    visibleTasks: [],
+    tasks: [
+      {
+        id: Math.random(),
+        desc: "Comprar Livro de React Native",
+        estimateAt: new Date(),
+        doneAt: new Date()
+      },
+      {
+        id: Math.random(),
+        desc: "Ler Livro de React Native",
+        estimateAt: new Date(),
+        doneAt: null
+      }
+    ]
+  };
+
+  componentDidMount = () => {
+    this.filterTasks();
+  };
+
+  toggleFilter = () => {
+    this.setState(
+      { showDoneTasks: !this.state.showDoneTasks },
+      this.filterTasks
+    );
+  };
+
+  isPending = task => {
+    return task.doneAt === null;
+  };
+
+  filterTasks = () => {
+    let visibleTasks = null;
+    if (this.state.showDoneTasks) {
+      visibleTasks = [...this.state.tasks];
+    } else {
+      visibleTasks = this.state.tasks.filter(this.isPending);
+    }
+    this.setState({ visibleTasks });
+  };
+
+  toggleTask = taskId => {
+    const tasks = [...this.state.tasks];
+    tasks.forEach(task => {
+      if (task.id === taskId) {
+        task.doneAt = task.doneAt ? null : new Date();
+      }
+    });
+
+    this.setState({ tasks }, this.filterTasks);
+  };
+
   render() {
     const today = moment()
       .locale("pt-br")
@@ -14,18 +78,28 @@ export default class TaskList extends Component {
     return (
       <View style={styles.container}>
         <ImageBackground source={TodayImage} style={styles.taskList}>
+          <View style={styles.iconBar}>
+            <TouchableOpacity onPress={this.toggleFilter}>
+              <Icon
+                name={this.state.showDoneTasks ? "eye" : "eye-slash"}
+                size={20}
+                color={commonStyles.colors.secondary}
+              />
+            </TouchableOpacity>
+          </View>
           <View style={styles.titleBar}>
             <Text style={styles.title}>Hoje</Text>
             <Text style={styles.subtitle}>{today}</Text>
           </View>
         </ImageBackground>
         <View style={styles.taskList}>
-          <Task
-            desc="Comprar Livro React"
-            estimateAt={new Date()}
-            doneAt={new Date()}
+          <FlatList
+            data={this.state.visibleTasks}
+            keyExtractor={item => `${item.id}`}
+            renderItem={({ item }) => (
+              <Task {...item} toggleTask={this.toggleTask} />
+            )}
           />
-          <Task desc="Ler Livro React" estimateAt={new Date()} doneAt={null} />
         </View>
       </View>
     );
@@ -59,5 +133,11 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginBotton: 30,
     fontSize: 20
+  },
+  iconBar: {
+    flexDirection: "row",
+    marginHorizontal: 20,
+    justifyContent: "flex-end",
+    marginTop: Platform.OS === "ios" ? 40 : 10
   }
 });
